9ca2760cd281a0f0d604402cf4810069659ead3e
Lol McBride <3874987+lolmc@users.noreply.github.com>
2024-10-29 11:55:35 +0000

    WSL2 Ubuntu Changes

--- nvim/init.lua
+++ nvim/init.lua
@@ -279,9 +279,6 @@ require('lazy').setup({
   -- you do for a plugin at the top level, you can do for a dependency.
   --
   -- Use the `dependencies` key to specify the dependencies of a particular plugin
-  { -- nvim-webdevicons
-    'nvim-tree/nvim-web-devicons',
-  },
   { -- which-key plugin
     'folke/which-key.nvim',
     event = 'VeryLazy',
